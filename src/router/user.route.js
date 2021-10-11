1cd5454bcfb3faadef216fd14a67219f787db757
brojie <jj112358@163.com>
2021-10-11 10:05:57 +0800

    8_请求参数解析(上)

--- src/router/user.route.js
+++ src/router/user.route.js
@@ -2,12 +2,29 @@
 // 3.1 导入koa-router包
 const Router = require('koa-router')
 // 3.2 实例化router对象
-const router = new Router()
+const router = new Router({ prefix: '/users' })
+const db = [
+  { id: 1, name: 'xiaoming', age: 20 },
+  { id: 2, name: 'xiaomei', age: 18 },
+  { id: 3, name: 'xiaopang', age: 2 },
+]
 // 3.3 编写路由规则
-router.get('/users', (ctx) => {
-  ctx.body = '这是用户页'
+// GET /users ---- 获取所有的用户信息, 返回一个数组
+router.get('/', (ctx) => {
+  // 解析键值对
+  ctx.body = db
 })
-router.post('/users', (ctx) => {
+// GET /users/:id ---- 根据id获取单个用户的信息, 返回一个对象
+router.get('/:id', (ctx) => {
+  // 解析id参数
+  const id = ctx.params.id
+  const res = db.filter((item) => item.id == id)
+
+  if (!res[0]) ctx.throw(404)
+
+  ctx.body = res[0]
+})
+router.post('/', (ctx) => {
   ctx.body = '创建用户'
 })
 
